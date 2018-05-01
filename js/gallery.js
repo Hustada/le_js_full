cd94d85e687e99c6b6c651e9dc127a31a95713d1
Mark Hustad <mark.hustad@firespring.com>
2018-05-01 15:36:43 -0500

    done

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,27 @@
 function activateGallery() {
-	alert("Hello from the gallery file!");
+  // get main image inside div with CSS id 'gallery-photo'
+	let thumbnails = document.querySelector("#gallery-thumbs").querySelectorAll("img")
+	let mainImage = document.querySelector("#gallery-photo img");
+
+	thumbnails.forEach(function(thumbnail) {
+		thumbnail.addEventListener("click", function() {
+			//select clicked image as main image
+			let newImageSrc = thumbnail.dataset.largeVersion;
+			mainImage.setAttribute("src", newImageSrc);
+
+      let currentClass = "current";
+      //change which image is current(move orange "border" to selected image)
+      document.querySelector(".current").classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+
+      // Update image info.
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+
+		});
+	});
 }
